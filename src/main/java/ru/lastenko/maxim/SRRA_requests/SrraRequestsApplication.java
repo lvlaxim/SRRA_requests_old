753c4cd684d6b266863b01db7a5c4f9b4f16182e
lvlaxim <lvlaxim@yandex.ru>
2019-07-12 15:09:52 +0300

    Create new project

---
+++ src/main/java/ru/lastenko/maxim/SRRA_requests/SrraRequestsApplication.java
@@ -0,0 +1,13 @@
+package ru.lastenko.maxim.SRRA_requests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrraRequestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrraRequestsApplication.class, args);
+	}
+
+}
