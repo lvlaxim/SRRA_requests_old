b673657bd7dad35d153b27c7e5606c83b9c976c5
lvlaxim <lvlaxim@yandex.ru>
2019-07-18 14:49:22 +0300

    Add payment entity

---
+++ src/main/java/ru/lastenko/maxim/SRRA_requests/entity/Payment.java
@@ -0,0 +1,51 @@
+package ru.lastenko.maxim.SRRA_requests.entity;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "payments", schema = "requests")
+public class Payment {
+
+    @Id
+    @SequenceGenerator(name = "payments_payment_id_seq", sequenceName = "requests.payments_payment_id_seq")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "payments_payment_id_seq")
+    @Column(name = "payment_id")
+    private Integer id;
+
+    @Column(name = "payment")
+    private String type;
+
+    public Payment() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Payment payment = (Payment) o;
+        return id.equals(payment.id) &&
+                type.equals(payment.type);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, type);
+    }
+}
