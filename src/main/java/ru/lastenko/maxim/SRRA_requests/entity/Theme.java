d52136ba42e237518cf49a9722ce8ff95b4f5585
lvlaxim <lvlaxim@yandex.ru>
2019-07-22 12:15:34 +0300

    Add theme entity

---
+++ src/main/java/ru/lastenko/maxim/SRRA_requests/entity/Theme.java
@@ -0,0 +1,56 @@
+package ru.lastenko.maxim.SRRA_requests.entity;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "executors", schema = "requests")
+public class Theme {
+
+    @Id
+    @SequenceGenerator(name = "executors_executor_id_seq", sequenceName = "requests.executors_executor_id_seq")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "executors_executor_id_seq")
+    @Column(name = "executor_id")
+    private Integer id;
+
+    @Column(name = "theme")
+    private String name;
+
+    public Theme() {
+    }
+
+    public Theme(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Theme theme = (Theme) o;
+        return id.equals(theme.id) &&
+                name.equals(theme.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
