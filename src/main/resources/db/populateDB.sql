60c7457bb12a89809f168f5705044696236ed3dc
lvlaxim <lvlaxim@yandex.ru>
2019-07-23 16:27:24 +0300

    Add requests table population

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,4 +1,6 @@
 DELETE
+FROM requests.requests;
+DELETE
 FROM requests.departments;
 DELETE
 FROM requests.executors;
@@ -13,6 +15,7 @@ FROM requests.sources;
 DELETE
 FROM requests.themes;
 
+
 ALTER SEQUENCE requests.departments_department_id_seq RESTART WITH 1;
 ALTER SEQUENCE requests.executors_executor_id_seq RESTART WITH 1;
 ALTER SEQUENCE requests.payments_payment_id_seq RESTART WITH 1;
@@ -20,6 +23,7 @@ ALTER SEQUENCE requests.prices_price_id_seq RESTART WITH 1;
 ALTER SEQUENCE requests.rubrics_rubric_id_seq RESTART WITH 1;
 ALTER SEQUENCE requests.sources_source_id_seq RESTART WITH 1;
 ALTER SEQUENCE requests.themes_theme_id_seq RESTART WITH 1;
+ALTER SEQUENCE requests.requests_request_id_seq RESTART WITH 1;
 
 -- ALTER SEQUENCE requests. RESTART WITH 1;
 
@@ -52,3 +56,14 @@ VALUES ('Источник1'),
 INSERT INTO requests.themes(theme)
 VALUES ('Тема1'),
        ('Тема2');
+
+INSERT INTO requests.requests(rubric_id, theme_id, subject, short_request, short_answer, source_id, is_urgent, is_gpw,
+                              is_entity, is_consular, receiver, receipt_date, department_id, working_by, start_date,
+                              executor_id, end_date, smav, reg_number, in_number, in_num_from_org, in_date, copy_number,
+                              payment_id)
+VALUES (1, 1, 'Вопрос1', 'Коротко1', 'Ответ1', 1, false, false, false, false, 1, '2019-07-01', 1, 1, '2019-07-01', 1,
+        '2019-07-02', 1, 1, 1, 1, '2019-07-01', 1, 1),
+       (2, 2, 'Вопрос2', 'Коротко2', 'Ответ2', 2, false, false, false, false, 2, '2019-07-02', 2, 2, '2019-07-02', 2,
+        '2019-07-03', 2, 2, 2, 2, '2019-07-02', 2, 2),
+       (1, 2, 'Вопрос3', 'Коротко3', 'Ответ3', 1, false, false, false, false, 3, '2019-07-03', 2, 3, '2019-07-03', 3,
+        '2019-07-04', 3, 3, 3, 3, '2019-07-03', 3, 3);
