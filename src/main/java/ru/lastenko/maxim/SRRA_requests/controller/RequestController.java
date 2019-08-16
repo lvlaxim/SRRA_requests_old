fc15e0ae2d96061621568f8033cd46eb8c2d04f9
lvlaxim <lvlaxim@yandex.ru>
2019-08-16 18:14:18 +0300

    Add reference values as model attributes

--- src/main/java/ru/lastenko/maxim/SRRA_requests/controller/RequestController.java
+++ src/main/java/ru/lastenko/maxim/SRRA_requests/controller/RequestController.java
@@ -3,14 +3,12 @@ package ru.lastenko.maxim.SRRA_requests.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-import ru.lastenko.maxim.SRRA_requests.Service.RequestService;
+import ru.lastenko.maxim.SRRA_requests.Service.*;
 import ru.lastenko.maxim.SRRA_requests.entity.Request;
 import ru.lastenko.maxim.SRRA_requests.util.Pager;
 
@@ -22,11 +20,24 @@ public class RequestController {
 
     private static final int BUTTONS_TO_SHOW = 5;
     private static final int INITIAL_PAGE = 0;
-    private static final int INITIAL_PAGE_SIZE = 5;
+    private static final int INITIAL_PAGE_SIZE = 10;
     private static final int[] PAGE_SIZES = {5, 10, 20};
 
     @Autowired
-    private RequestService service;
+    private RequestService requestService;
+    @Autowired
+    private RubricService rubricService;
+    @Autowired
+    private ThemeService themeService;
+    @Autowired
+    private SourceService sourceService;
+    @Autowired
+    private ExecutorService executorService;
+    @Autowired
+    private DepartmentService departmentService;
+    @Autowired
+    private PaymentService paymentService;
+
 
     /**
      * Handles all requests
@@ -43,15 +54,16 @@ public class RequestController {
 
         // Evaluate page size. If requested parameter is null, return initial
         // page size
-        int evalPageSize = pageSize.orElse(INITIAL_PAGE_SIZE);
+        int evalPageSize = INITIAL_PAGE_SIZE; //pageSize.orElse(INITIAL_PAGE_SIZE);
 
         // Evaluate page. If requested parameter is null or less than 0 (to
         // prevent exception), return initial size. Otherwise, return value of
         // param. decreased by 1.
         int evalPage = (page.orElse(0) < 1) ? INITIAL_PAGE : page.get() - 1;
 
-        Page<Request> requests = service.getAllOrderedByIdDesc(PageRequest.of(evalPage, evalPageSize));
+        Page<Request> requests = requestService.getAllOrderedByIdDesc(PageRequest.of(evalPage, evalPageSize));
         Pager pager = new Pager(requests.getTotalPages(), requests.getNumber(), BUTTONS_TO_SHOW);
+
 
         modelAndView.addObject("requests", requests);
         modelAndView.addObject("selectedPageSize", evalPageSize);
@@ -62,8 +74,16 @@ public class RequestController {
 
     @GetMapping("/requests/{id}")
     public ModelAndView view(@PathVariable Integer id) {
+
         ModelAndView modelAndView = new ModelAndView("request");
-        modelAndView.addObject("request", service.getById(id));
+
+        modelAndView.addObject("request", requestService.getById(id));
+        modelAndView.addObject("rubrics", rubricService.getAll());
+        modelAndView.addObject("themes", themeService.getAll());
+        modelAndView.addObject("sources", sourceService.getAll());
+        modelAndView.addObject("executors", executorService.getAll());
+        modelAndView.addObject("departments", departmentService.getAll());
+        modelAndView.addObject("payments", paymentService.getAll());
         return modelAndView;
     }
 }
