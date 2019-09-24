02e8d6a58421f03ff9d20230d4853869b7bb737b
lvlaxim <lvlaxim@yandex.ru>
2019-09-24 15:28:28 +0300

    Add Request DTO

---
+++ src/main/java/ru/lastenko/maxim/SRRA_requests/dto/RequestDto.java
@@ -0,0 +1,39 @@
+package ru.lastenko.maxim.SRRA_requests.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.lastenko.maxim.SRRA_requests.entity.*;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class RequestDto {
+
+    private Integer id;
+    private Rubric rubric;
+    private Theme theme;
+    private String subject;
+    private String shortRequest;
+    private String shortAnswer;
+    private Source source;
+    private boolean urgent;
+    private boolean GPW;
+    private boolean entity;
+    private boolean consular;
+    private Executor receiver;
+    private LocalDate receiptDate;
+    private LocalDate startDate;
+    private Executor executor;
+    private LocalDate endDate;
+    private Integer smav;
+    private Integer outNumber;
+    private String inNumFromOrg;
+    private LocalDate inDate;
+    private Integer copyNumber;
+    private Payment payment;
+    private Boolean isChangeable;
+
+}
